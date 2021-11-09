4da3cbcf170ee574fe39d302560c0937a6c3456a
wijay58 <supun.wijemanna58@gmail.com>
2021-11-09 15:45:53 +0530

    db

--- routes/db.js
+++ routes/db.js
@@ -3,7 +3,7 @@ const autoIncrement = require('mongoose-auto-increment');
 
 try {
     const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true};
-    let url = "mongodb://localhost/app"
+    let url = "mongodb+srv://supun:supun123@india-mumbai.ptqnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     mongoose.connect(url, connectionOptions); 
 } catch (error) {
     console.error('Database not connected')
