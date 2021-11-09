669eb3a708a0859e77408f466bc97ffab8e37bae
wijay58 <supun.wijemanna58@gmail.com>
2021-11-09 15:52:14 +0530

    db con

--- routes/db.js
+++ routes/db.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
-const autoIncrement = require('mongoose-auto-increment');
-
-try {
-    const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true};
-    let url = "mongodb+srv://supun:supun123@india-mumbai.ptqnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    mongoose.connect(url, connectionOptions); 
-} catch (error) {
-    console.error('Database not connected')
-}
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://supun:supun123@india-mumbai.ptqnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
